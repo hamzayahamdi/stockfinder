caac0bc8c0cede6d5e7c5155bc3aafd8c64bf142
hamza yahamdi <hamzayahamdi@192.168.0.119>
2024-10-09 12:36:22 +0100

    Updates

--- _next/static/gqjn1JCgYzaRKrlPd8Jfy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/404":["static/chunks/pages/404-66027ebe856cccfd.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],"/products":["static/chunks/pages/products-9a5b544adb2db88b.js"],sortedPages:["/404","/_app","/_error","/products"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
